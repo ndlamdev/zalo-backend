3f19bb6fd17465f814069d0f10c7d90f4ff6f22f
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2025-07-13 17:07:44 +0700

    feat: init module notification

---
+++ notification/src/test/kotlin/com/lamnguyen/notification/NotificationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lamnguyen.notification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
