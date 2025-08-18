f94f30724891ee4514a5d7750a8b48d036b35939
Nguyễn Đình Lam <ndlam.dev@gmail.com>
2025-08-18 13:07:44 +0700

    per: update database

--- chat/src/main/resources/db/postgres/migration/V1__init_database.sql
+++ chat/src/main/resources/db/postgres/migration/V1__init_database.sql
@@ -14,8 +14,9 @@ CREATE TABLE "room_chats"
 
 CREATE TABLE "room_chat_members"
 (
-    phone_number VARCHAR(255) NOT NULL,
-    rom_chat_id  BIGINT       NOT NULL,
+    id           BIGSERIAL   NOT NULL PRIMARY KEY,
+    phone_number VARCHAR(20) NOT NULL,
+    rom_chat_id  BIGINT      NOT NULL,
     username     VARCHAR(255),
     display_name VARCHAR(255),
     created_at   TIMESTAMP,
@@ -24,6 +25,37 @@ CREATE TABLE "room_chat_members"
     updated_by   VARCHAR(255),
     locked       BOOL DEFAULT false,
     deleted      BOOL DEFAULT false,
-    CONSTRAINT "rom_chat_members_rom_chats_fk" FOREIGN KEY (rom_chat_id) REFERENCES room_chats ("id"),
-    CONSTRAINT "rom_chat_members_pk" PRIMARY KEY (phone_number, rom_chat_id)
+    CONSTRAINT "rom_chat_members_rom_chats_fk" FOREIGN KEY (rom_chat_id) REFERENCES room_chats ("id")
+);
+
+CREATE TABLE "pin_room_chats"
+(
+    id           BIGSERIAL   NOT NULL PRIMARY KEY,
+    phone_number VARCHAR(20) NOT NULL,
+    rom_chat_id  BIGINT      NOT NULL,
+    created_at   TIMESTAMP,
+    created_by   VARCHAR(255),
+    updated_at   TIMESTAMP,
+    updated_by   VARCHAR(255),
+    locked       BOOL DEFAULT false,
+    deleted      BOOL DEFAULT false,
+    CONSTRAINT "pin_room_chats_rom_chats_fk" FOREIGN KEY (rom_chat_id) REFERENCES room_chats ("id")
+);
+
+CREATE TABLE "messages"
+(
+    id                  BIGSERIAL   NOT NULL PRIMARY KEY,
+    owner_phone_number  VARCHAR(20) NOT NULL,
+    sender_phone_number VARCHAR(20) NOT NULL,
+    rom_chat_id         BIGINT      NOT NULL,
+    content             TEXT,
+    type                VARCHAR(255),
+    url_media           VARCHAR(255),
+    created_at          TIMESTAMP,
+    created_by          VARCHAR(255),
+    updated_at          TIMESTAMP,
+    updated_by          VARCHAR(255),
+    locked              BOOL DEFAULT false,
+    deleted             BOOL DEFAULT false,
+    CONSTRAINT "message_rom_chats_fk" FOREIGN KEY (rom_chat_id) REFERENCES room_chats ("id")
 );
--- user/src/main/resources/db/postgres/migration/V1__init_database.sql
+++ user/src/main/resources/db/postgres/migration/V1__init_database.sql
@@ -13,23 +13,25 @@ CREATE TABLE "users"
     deleted      BOOL DEFAULT false
 );
 
-CREATE TABLE "friend_ships"
+CREATE TABLE "friendships"
 (
-    phone_number_user_1 VARCHAR(255) NOT NULL,
-    phone_number_user_2 VARCHAR(255) NOT NULL,
+    id                  BIGSERIAL PRIMARY KEY,
+    owner_phone_number  VARCHAR(255) NOT NULL,
+    friend_phone_number VARCHAR(255) NOT NULL,
+    display_name        VARCHAR(255),
     created_at          TIMESTAMP,
     created_by          VARCHAR(255),
     updated_at          TIMESTAMP,
     updated_by          VARCHAR(255),
     locked              BOOL DEFAULT false,
     deleted             BOOL DEFAULT false,
-    CONSTRAINT "friend_ships_pk" PRIMARY KEY (phone_number_user_1, phone_number_user_2),
-    CONSTRAINT "friend_ships_users_phone_number_1_fk" FOREIGN KEY (phone_number_user_1) REFERENCES users (phone_number),
-    CONSTRAINT "friend_ships_users_phone_number_2_fk" FOREIGN KEY (phone_number_user_2) REFERENCES users (phone_number)
+    CONSTRAINT "friend_ships_users_owner_phone_number_fk" FOREIGN KEY (owner_phone_number) REFERENCES users (phone_number),
+    CONSTRAINT "friend_ships_users_friend_phone_number_fk" FOREIGN KEY (friend_phone_number) REFERENCES users (phone_number)
 );
 
 CREATE TABLE "invite_add_friends"
 (
+    id                    BIGSERIAL    NOT NULL PRIMARY KEY,
     phone_number_sender   VARCHAR(255) NOT NULL,
     phone_number_receiver VARCHAR(255) NOT NULL,
     accepted              BOOL              default false,
@@ -40,8 +42,6 @@ CREATE TABLE "invite_add_friends"
     updated_by            VARCHAR(255),
     locked                BOOL              DEFAULT false,
     deleted               BOOL              DEFAULT false,
-    CONSTRAINT "invite_add_friends_pk" PRIMARY KEY (phone_number_sender, phone_number_receiver),
     CONSTRAINT "friend_ships_users_phone_number_sender_fk" FOREIGN KEY (phone_number_sender) REFERENCES users (phone_number),
     CONSTRAINT "friend_ships_users_phone_number_receiver_fk" FOREIGN KEY (phone_number_receiver) REFERENCES users (phone_number)
-
 );
